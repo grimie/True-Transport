5c08a0fa95b795a91802f94bfb62b2e404f4f4e8
grimie <grim.mostrom@hotmail.com>
2018-02-01 10:44:59 +0100

    framework

---
+++ public/js/deli-very.js
@@ -0,0 +1,38 @@
+/*jslint es5:true, indent: 2 */
+/*global Vue, io */
+/* exported vm */
+'use strict';
+var socket = io();
+
+var vm = new Vue({
+  el: '#dots',
+  data: {
+    orders: {},
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+  methods: {
+    getNext: function () {
+      var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function (event) {
+      var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+      socket.emit("addOrder", { orderId: this.getNext(),
+                                details: { x: event.clientX - 10 - offset.x,
+                                           y: event.clientY - 10 - offset.y },
+                                orderItems: ["Beans", "Curry"]
+                              });
+    }
+  }
+});
---
+++ public/js/dispatcher.js
@@ -0,0 +1,21 @@
+/*jslint es5:true, indent: 2 */
+/*global Vue, io */
+/* exported vm */
+'use strict';
+var socket = io();
+
+var vm = new Vue({
+  el: '#orders',
+  data: {
+    orders: {},
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  }
+});
